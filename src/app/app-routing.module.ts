4241005b7102d246eea0cc8073c1088661fa0184
Otavio <otavioabottin@outlook.com>
2019-11-22 21:10:30 -0300

    Configurações Bootstrap e Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
+import { NovoAutorComponent } from './novo-autor/novo-autor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path : 'listagem', component: ListaClienteComponent},
+  {path: 'novo', component: NovoAutorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
